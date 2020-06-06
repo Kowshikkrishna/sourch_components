fe32198b5bce654f7fa26e00ac55a9264611eef9
Praneeth <johndoeknown@protonmail.com>
2020-06-06 09:29:49 +0530

    added appbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,5 @@
-<h1>Hello World</h1>
+<script>
+    import Appbar from "./components/Appbar.svelte";
+</script>
+
+<Appbar title={"Sourch"} routes={[{link: "/", label: "First Link"}, {link: "/", label: "Second Link"}, {link: "/", label: "Third Link"}]}/>
---
+++ src/components/Appbar.svelte
@@ -0,0 +1,39 @@
+<script>
+    export let title;
+    export let routes;
+</script>
+
+<div class="appbar">
+    <h1>{title}</h1>
+    <nav>
+        {#each routes as route}
+            <a href={route.link}>{route.label}</a>
+        {/each}
+    </nav>
+</div>
+
+<style>
+    .appbar {
+        border: 2px solid black;
+        text-align: center;
+        padding-bottom: 15px;
+        font-size: 20px;
+        margin: 10px;
+    }
+
+    a {
+        text-decoration: none;
+        color: black;
+        padding: 10px;
+        transition: font-size ease-in 300ms;
+    }
+
+    a:hover {
+        border: 2px solid black;
+        font-size: 25px;
+    }
+
+    a:active {
+        font-size: 23px;
+    }
+</style>
