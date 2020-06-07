502877b4ab340ba757a1b8fa8dec0603e66069b6
Kowshikkrishna <kowshikkrishna4@gmail.com>
2020-06-07 13:43:43 +0530

    added buttons

---
+++ src/components/OutlineButton.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let caption;
+  export let href;
+  export let fn;
+</script>
+
+
+{#if href}
+  <a {href}>{caption}</a>
+{:else}
+  <button on:click={fn}>{caption}</button>
+{/if}
+
+
+<style>
+  button,
+  a {
+    font: 20px;
+    border: 2px solid #000000;
+    padding: 0.5rem 1rem;
+    box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.26);
+    cursor: pointer;
+    text-decoration: none;
+    background: transparent;
+    color: #000000;
+    box-shadow: none;
+  }
+
+  button:hover,
+  a:hover {
+    background: #cecece;
+    box-shadow: none;
+  }
+</style>
---
+++ src/components/RaisedButton.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let caption;
+  export let href;
+  export let fn;
+</script>
+
+{#if href}
+  <a {href}>{caption}</a>
+{:else}
+  <button on:click={fn}>{caption}</button>
+{/if}
+
+<style>
+  button,
+  a {
+    font: 20px;
+    border: 2px solid #000000;
+    background: #000000;
+    padding: 0.5rem 1rem;
+    color: rgb(250, 245, 245);
+    border-radius: 2px;
+    box-shadow: 1px 1px 3px rgba(255, 255, 255, 0.26);
+    cursor: pointer;
+    text-decoration: none;
+  }
+
+  button:focus {
+    outline: none;
+  }
+
+  button:hover,
+  a:hover {
+    background: #161616;
+    border-color: #161616;
+    box-shadow: 1px 1px 8px rgba(59, 59, 59, 0.26);
+  }
+</style>
